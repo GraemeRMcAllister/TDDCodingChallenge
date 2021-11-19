6a17592049615eb004910fbe935b882f4f1ec0de
Graeme Mcallister <graeme.mcallister07@gmail.com>
2021-11-19 02:40:06 +0000

    Creating Card, ParkOfCards, PackOfCardsCreator classes to use interfaces

---
+++ src/CardGame/Card.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenge.CardGame
+{
+    class Card : ICard
+    {
+        public Suit Suit => throw new NotImplementedException();
+
+        public Value Value => throw new NotImplementedException();
+
+        public bool Equals(ICard other) => throw new NotImplementedException();
+    }
+}
