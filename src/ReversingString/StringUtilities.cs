7d510ae0bdd68f1fbf3840311dee5c011d7ea9cb
Graeme Mcallister <graeme.mcallister07@gmail.com>
2021-11-19 02:33:17 +0000

    Working String reverse

--- src/ReversingString/StringUtilities.cs
+++ src/ReversingString/StringUtilities.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace CodingChallenge.ReversingString
 {
@@ -6,7 +7,30 @@ namespace CodingChallenge.ReversingString
     {
         public static string Reverse(string s)
         {
-            throw new NotImplementedException();
+
+            char[] sAarry = s.ToCharArray();
+
+
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+
+                char temp = sAarry[i];
+
+                sAarry[i] = sAarry[sAarry.Length - 1 - i];
+
+                sAarry[sAarry.Length - 1 - i] = temp;
+
+            }
+            return new string(sAarry);
         }
+
+        //public static string Reverse(string s)
+        //{
+        //    char[] result = s.ToCharArray();
+
+        //   Array.Reverse(result);
+
+        //    return new string(sAarry);
+        //}
     }
 }
